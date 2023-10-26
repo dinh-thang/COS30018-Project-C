c4d97cacd08b6831b21d6cd56956ed740203a116
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2023-10-26 11:45:06 +1100

    Delete ui directory

--- ui/READ_ME.txt
+++
@@ -1,2 +0,0 @@
-From this directory, run
-    pip install -r requirements.txt
--- ui/model_api.py
+++
@@ -1,8 +0,0 @@
-from langchain import HuggingFaceHub
-
-API_KEY = ""
-MODEL_SRC = "ThangDinh/qthang-finetuned"
-
-llm = HuggingFaceHub(repo_id=MODEL_SRC, huggingfacehub_api_token=API_KEY)
-
-print(llm("Tell me a joke about data scientist"))
